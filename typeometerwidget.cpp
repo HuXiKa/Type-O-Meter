d62af8987fc993a1138a631f9d245d2d4a6cf77a
Hucker David <hucker.dav@gmail.com>
2012-04-05 19:54:51 +0200

    Signed-off-by: Hucker David <hucker.dav@gmail.com>

--- typeometerwidget.cpp
+++ typeometerwidget.cpp
@@ -9,7 +9,7 @@
 TypeOMeterWidget::TypeOMeterWidget(QWidget *parent)
     : QWidget(parent, Qt::FramelessWindowHint | Qt::WindowSystemMenuHint /*| Qt::WindowStaysOnTopHint*/),
       ui(new Ui::TypeOMeterWidget),
-    m_NextSessionKeyPressCount(0), m_NextSessionMousePressCount(0), m_SessionKeyPressCount(0), m_SessionMousePressCount(0),
+      m_SessionKeyPressCount(0), m_SessionMousePressCount(0),
       m_TotalKeyPressCount(0), m_TotalMousePressCount(0), m_StartTime(QTime::currentTime())
 {
     ui->setupUi(this);
@@ -26,15 +26,6 @@ TypeOMeterWidget::TypeOMeterWidget(QWidget *parent)
     connect(this, SIGNAL(APMChanged(int)), ui->currentAPMLcdNumber, SLOT(display(int)));
     connect(this, SIGNAL(totalAPMChanged(int)), ui->totalAPMLcdNumber, SLOT(display(int)));
 
-    connect(&m_Ticker, SIGNAL(timeout()), this, SLOT(restartTime()));
-    m_Ticker.start(RESTART_TIME);
-
-    QTimer *timer2 = new QTimer(this);
-    connect(timer2, SIGNAL(timeout()), this, SLOT(restartTime()));
-    timer2->start(RESTART_TIME);    
-    m_SessionStartTime = m_StartTime;
-
-
     //QPixmap pixmap(":/image/background");
     //setMask(QBitmap(pixmap));
 
@@ -48,10 +39,10 @@ TypeOMeterWidget::~TypeOMeterWidget()
 void TypeOMeterWidget::paintEvent(QPaintEvent *)
 {
     static const QPoint minuteHand[3] = {
-             QPoint(7, 8),
-             QPoint(-7, 8),
-             QPoint(0, -70)
-         };
+        QPoint(7, 8),
+        QPoint(-7, 8),
+        QPoint(0, -70)
+    };
 
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing);
@@ -73,27 +64,46 @@ void TypeOMeterWidget::paintEvent(QPaintEvent *)
     painter.restore();
 }
 
-void TypeOMeterWidget::restartTime()
-{
-    m_Ticker.start(SHIFT_TIME);
-    m_SessionStartTime = m_SessionStartTime.addMSecs(SHIFT_TIME);
-    //qDebug() << "restartin session @ " << m_SessionStartTime.minute() << ":" << m_SessionStartTime.second();
-    m_SessionKeyPressCount = m_NextSessionKeyPressCount;
-    m_SessionMousePressCount = m_NextSessionMousePressCount;
-    m_NextSessionKeyPressCount = 0;
-    m_NextSessionMousePressCount = 0;
-
-}
-
 void TypeOMeterWidget::displayAPM()
 {
-    int difference = getElapsedTime();
-    m_SessionAPM = 0;
-    if (difference > 0)
-        m_SessionAPM = 60000 / difference * (m_SessionKeyPressCount + m_SessionMousePressCount);
+
+    int currentAPM = MINUTE / SESSION_TIME * (m_SessionKeyPressCount + m_SessionMousePressCount);
+
+    m_SessionAPM = currentAPM;
 
     //qDebug() << "KeyPress: " << m_SessionKeyPressCount << " mouse: " << m_SessionMousePressCount << " apm: " << apm << " difference: " << difference;
+
     emit APMChanged(m_SessionAPM);
-    m_AvarageAPM = ((double)(m_TotalKeyPressCount + m_TotalMousePressCount) / m_StartTime.elapsed()) * 60000;
+    m_AvarageAPM = ((double)(m_TotalKeyPressCount + m_TotalMousePressCount) / m_StartTime.elapsed()) * MINUTE;
     emit totalAPMChanged(m_AvarageAPM);
+}
+
+void TypeOMeterWidget::registerMouseClick()
+{
+    m_TotalMousePressCount++;
+    m_SessionMousePressCount++;
+    QTimer *timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()), this, SLOT(forgetMouseClick()));
+    timer->setSingleShot(true);
+    timer->start(SESSION_TIME);
+}
+
+void TypeOMeterWidget::forgetMouseClick()
+{
+    m_SessionMousePressCount--;
+}
+
+void TypeOMeterWidget::registerKeypress()
+{
+    m_TotalKeyPressCount++;
+    m_SessionKeyPressCount++;
+    QTimer *timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()), this, SLOT(forgetKeypress()));
+    timer->setSingleShot(true);
+    timer->start(SESSION_TIME);
+}
+
+void TypeOMeterWidget::forgetKeypress()
+{
+    m_SessionKeyPressCount--;
 }
--- typeometerwidget.h
+++ typeometerwidget.h
@@ -18,49 +18,31 @@ class TypeOMeterWidget : public QWidget
 public:
     explicit TypeOMeterWidget(QWidget *parent = 0);
     ~TypeOMeterWidget();
-    void paintEvent(QPaintEvent *);
-    /**
-      Returns the elapsed time since the current session started.
-    */
-    int getElapsedTime() { return -QTime::currentTime().msecsTo(m_SessionStartTime); }
+    void paintEvent(QPaintEvent *);    
 
 public slots:    
-    void registerMouseClick() {
-        m_TotalMousePressCount++;
-        m_SessionMousePressCount++;
-        if(m_SessionStartTime.elapsed() > SHIFT_TIME)
-            m_NextSessionKeyPressCount++;
-    }
+    void registerMouseClick();
+    void forgetMouseClick();
 
-    void registerKeypress() {
-        m_TotalKeyPressCount++;
-        m_SessionKeyPressCount++;
-        if(m_SessionStartTime.elapsed() > SHIFT_TIME)
-            m_NextSessionKeyPressCount++;
-    }
+    void registerKeypress();
+    void forgetKeypress();
 
     void displayAPM();
 
-    void restartTime();
 signals:
     void APMChanged(int APM);
     void totalAPMChanged(int APM);
 private:
     Ui::TypeOMeterWidget *ui;
-    static const int TICK_TIME = 20;
-    static const int RESTART_TIME = 6000;
-    static const int SHIFT_TIME = RESTART_TIME / 2;
-    int m_NextSessionKeyPressCount;
-    int m_NextSessionMousePressCount;
-    /**
-      Key presses in the current session.
-    */
+    static const int MINUTE = 60000;
+    static const int SESSION_TIME = MINUTE / 60 * 2;
+    static const int TICK_TIME = 20;        
+
     int m_SessionKeyPressCount;
     int m_SessionMousePressCount;
     int m_TotalKeyPressCount;
     int m_TotalMousePressCount;
-    QTime m_StartTime;
-    QTime m_SessionStartTime;
+    QTime m_StartTime;    
     QTimer m_Ticker;
     int m_AvarageAPM;
     int m_SessionAPM;
